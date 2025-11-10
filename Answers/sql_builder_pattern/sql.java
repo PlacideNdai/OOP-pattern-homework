316aafd9926ae8c12ab14a91438d319d2ae2d14b
Placide <75864037+PlacideNdai@users.noreply.github.com>
2025-11-10 14:47:11 -0600

    worked on SQL statement which we used builder to create a sql statement

---
+++ Answers/sql_builder_pattern/sql.java
@@ -0,0 +1,89 @@
+package Answers.sql_builder_pattern;
+
+public class sql {
+    private String select;
+    private String from;
+    private String where;
+    private String groupBy;
+    private String orderBy;
+    private int limit;
+    private String having;
+
+    private sql(Builder builder) {
+        this.select = builder.SELECT;
+        this.from = builder.FROM;
+        this.where = builder.WHERE;
+        this.groupBy = builder.GROUP_BY;
+        this.orderBy = builder.ORDER_BY;
+        this.limit = builder.LIMIT;
+        this.having = builder.HAVING;
+    }
+
+    public static class Builder {
+        private String SELECT;
+        private String FROM;
+        private String WHERE;
+        private String GROUP_BY;
+        private String ORDER_BY;
+        private int LIMIT;
+        private String HAVING;
+
+        public Builder select(String select) {
+            this.SELECT = select;
+            return this;
+        }
+
+        public Builder from(String from) {
+            this.FROM = from;
+            return this;
+        }
+
+        public Builder where(String where) {
+            this.WHERE = where;
+            return this;
+        }
+
+        public Builder groupBy(String groupBy) {
+            this.GROUP_BY = groupBy;
+            return this;
+        }
+
+        public Builder orderBy(String orderBy) {
+            this.ORDER_BY = orderBy;
+            return this;
+        }
+
+        public Builder limit(int limit) {
+            this.LIMIT = limit;
+            return this;
+        }
+
+        public Builder having(String having) {
+            this.HAVING = having;
+            return this;
+        }
+
+        public sql build() {
+
+            if (this.SELECT == null || this.SELECT.trim().isEmpty()) {
+                throw new IllegalStateException("SELECT clause is mandatory");
+            }
+
+            if (this.FROM == null || this.FROM.trim().isEmpty()) {
+                throw new IllegalStateException("FROM clause is mandatory");
+            }
+
+            return new sql(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "SQL Query: SELECT " + select + " FROM " + from +
+                (where != null ? " WHERE " : "") +
+                (groupBy != null ? " GROUP BY " : "") +
+                (orderBy != null ? " ORDER BY " : "") +
+                (limit > 0 ? " LIMIT " + limit : "") +
+                (having != null ? " HAVING " + having : "");
+    }
+}
---
+++ Answers/sql_builder_pattern/testsql.java
@@ -0,0 +1,16 @@
+package Answers.sql_builder_pattern;
+
+public class testsql {
+    public static void main(String[] args) {
+        new testsql();
+    }
+
+    public testsql() {
+        sql sql = new sql.Builder().select("*").from("users").where("UID = 1").build();
+        System.out.println(sql);
+
+        // this test should fail because of the missing FROM.
+        // sql sql1 = new sql.Builder().select("*").where("UID = 1").build();
+        // System.out.println(sql1);
+    }
+}
